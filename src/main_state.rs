9dd98f3591cdc2a4e6663b956ae5d3435655ed71
JacobMJones <mrjacobtaiwan@gmail.com>
2024-01-05 08:05:13 -0500

    finished adding constants

--- src/main_state.rs
+++ src/main_state.rs
@@ -7,19 +7,11 @@ use crate::collidable::Collidable;
 use ggez::{event, graphics, Context, GameResult};
 use gilrs::Gilrs;
 use crate::event_handler::EventHandler;
-const SCREEN_WIDTH: f32 = 1000.0;
-const SCREEN_HEIGHT: f32 = 1000.0;
-
-const CLUSTER_POINTS: &[(f32, f32); 5] = &[
-    (SCREEN_WIDTH * 0.15, SCREEN_HEIGHT * 0.16),   // Example near the top-left
-    (SCREEN_WIDTH * 0.8, SCREEN_HEIGHT * 0.8),     // Example near the bottom-right
-    (SCREEN_WIDTH * 0.01, SCREEN_HEIGHT * 0.8),    // Example near the bottom-left
-    (SCREEN_WIDTH * 0.2, SCREEN_HEIGHT * 0.01),    // Example near the top-center
-    (SCREEN_WIDTH * 0.5, SCREEN_HEIGHT * 0.8),     // Example near the middle-right
-];
+const COLLECTIBLE_SIZE: f32 = 100.0;
 const COLLECTIBLE_COUNT: i32 = 300;
 const CLUSTER_SIZE: f32 = 100.0;
-
+const PARTICLES_IN_SMOKE: i32 = 10;
+const PLAYER_TO_COLLECTIBLE_PROXIMITY_THRESHOLD: f32 = 400.0;
 pub struct MainState {
     event_handler: EventHandler,
     player: Player,
@@ -33,7 +25,7 @@ impl MainState {
     pub fn new(ctx: &mut Context, screen_width: f32, screen_height: f32) -> GameResult<MainState> {
 
         let cluster_points: &[(f32, f32); 5] = &[
-            (screen_width * 0.15, screen_height * 0.16),  // Adjust these as necessary
+            (screen_width * 0.15, screen_height * 0.16),  
             (screen_width * 0.8, screen_height * 0.8),
             (screen_width * 0.01, screen_height * 0.8),
             (screen_width * 0.2, screen_height * 0.01),
@@ -50,7 +42,7 @@ impl MainState {
 
         for i in 0..COLLECTIBLE_COUNT{
             // Choose a random cluster point
-            let (center_x, center_y) = CLUSTER_POINTS[rng.gen_range(0..CLUSTER_POINTS.len())];
+            let (center_x, center_y) = cluster_points[rng.gen_range(0..cluster_points.len())];
 
             // Generate positions near the cluster point
             let x = rng.gen_range(center_x as f32 - CLUSTER_SIZE..=center_x as f32 + CLUSTER_SIZE).clamp(50.0, 2000.0);
@@ -60,12 +52,12 @@ impl MainState {
             let initial_time = rng.gen_range(0.0..6.28);
 
             let id = format!("collect{}", i);
-            collectibles.push(Collectible::new(ctx, x, y, 70.0, initial_time, id, false)?);
+            collectibles.push(Collectible::new(ctx, x, y, COLLECTIBLE_SIZE, initial_time, id, false)?);
         }
 
         //Initialize multiple smoke effects and put them into a pool
         let mut smoke_effect_pool = Vec::new();
-        for _ in 0..30 { // For example, pre-create 10 effects
+        for _ in 0..PARTICLES_IN_SMOKE { 
             smoke_effect_pool.push(SmokeEffect::new_inactive()); 
         }   
 
@@ -89,7 +81,7 @@ impl event::EventHandler<ggez::GameError> for MainState {
         self.event_handler.process_events(&mut self.player);
 
         // Define the proximity threshold for collectibles
-        const PROXIMITY_THRESHOLD: f32 = 500.0;
+
 
         // Get a trait object for player as collidable
         let player_collidable: &dyn Collidable = &self.player;
@@ -98,17 +90,16 @@ impl event::EventHandler<ggez::GameError> for MainState {
         let proximity_and_collisions = handle_proximity_and_collisions(
             &[player_collidable], 
             &self.collectibles.iter().collect::<Vec<&Collectible>>(),
-            PROXIMITY_THRESHOLD
+            PLAYER_TO_COLLECTIBLE_PROXIMITY_THRESHOLD
         );
 
         let mut to_remove = Vec::new();
 
         for (_player_index, collectible_index, distance, is_collided) in proximity_and_collisions {
-            if distance < PROXIMITY_THRESHOLD / 2.0 {
+            if distance < PLAYER_TO_COLLECTIBLE_PROXIMITY_THRESHOLD / 2.0 {
                 // Assuming collectible_index is the index of the collectible in the collectibles vector
                 if let Some(collectible) = self.collectibles.get_mut(collectible_index) {
                     collectible.set_in_proximity(true);
-                   // println!("Collectible {} is within proximity threshold: {}", collectible_index, collectible.in_proximity);
                 }
             } else {
                 // If the collectible is not in proximity, reset its in_proximity variable
@@ -122,24 +113,21 @@ impl event::EventHandler<ggez::GameError> for MainState {
                 to_remove.push(collectible_index);
             }
         }
-        // Remove the collectibles that collided
+        // Remove the collectibles that collided with the player
         for index in to_remove.iter().rev() {
             if let Some(collectible) = self.collectibles.get_mut(*index) {
                 collectible.activate_smoke_effect(&mut self.smoke_effect_pool);
                 self.collectibles.remove(*index);
             }
         }
-
         // Update each collectible
         for collectible in &mut self.collectibles {
             collectible.update(ctx, dt);
         }
-
         // Update all smoke effects
         for effect in &mut self.smoke_effect_pool {
             effect.update(dt);
         }
-
         // Update the player
         self.player.update(dt);
 
@@ -153,16 +141,15 @@ impl event::EventHandler<ggez::GameError> for MainState {
         for collectible in &self.collectibles {
             collectible.draw(ctx)?;
         }
-
         // Draw each active smoke effect
         for effect in &self.smoke_effect_pool {
             if effect.is_active() {
                 effect.draw(ctx)?;
             }
         }
-
         // Draw the player
         self.player.draw(ctx)?;
+
 
         graphics::present(ctx)
     }
